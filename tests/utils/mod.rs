923f454ab494370f6c8475b52a1ea2b8c0ba6a45
khai96_ <hvksmr1996@gmail.com>
2022-01-22 14:51:59 +0700

    test: Simplify utils

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,10 +1,4 @@
-use std::{
-    collections::HashMap,
-    sync::{
-        atomic::{AtomicUsize, Ordering::Relaxed},
-        Mutex,
-    },
-};
+use std::sync::Mutex;
 
 /// Creation or destruction of a generation within a family line.
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -13,54 +7,42 @@ pub enum RecordItem {
     Drop(usize),
 }
 
-/// ID to assign to new family lines.
-static NEXT_FAMILY: AtomicUsize = AtomicUsize::new(0);
-lazy_static::lazy_static! {
-    /// All records of every family lines.
-    static ref FAMILY_RECORDS: Mutex<HashMap<usize, Vec<RecordItem>>> = Default::default();
-}
-
 impl RecordItem {
     /// Add the item to a family record.
-    fn record(self, family: usize) {
-        FAMILY_RECORDS
-            .lock()
-            .unwrap()
-            .entry(family)
-            .or_default()
-            .push(self);
+    fn record(self, record: &Mutex<Vec<RecordItem>>) {
+        record.lock().expect("get record to write to").push(self);
     }
 }
 
 /// A member of a family.
 #[derive(Debug)]
-pub struct FamilyMember {
-    family: usize,
+pub struct FamilyMember<'a> {
+    record: &'a Mutex<Vec<RecordItem>>,
     generation: usize,
 }
 
-impl Default for FamilyMember {
-    fn default() -> Self {
-        let family = NEXT_FAMILY.fetch_add(1, Relaxed);
-        RecordItem::Create(0).record(0);
+impl<'a> FamilyMember<'a> {
+    /// Spawn a new family line.
+    pub fn new(record: &'a Mutex<Vec<RecordItem>>) -> Self {
+        RecordItem::Create(0).record(record);
         FamilyMember {
-            family,
+            record,
             generation: 0,
         }
     }
 }
 
-impl Clone for FamilyMember {
+impl<'a> Clone for FamilyMember<'a> {
     fn clone(&self) -> Self {
-        let family = self.family;
+        let record = self.record;
         let generation = self.generation + 1;
-        RecordItem::Create(generation).record(family);
-        FamilyMember { family, generation }
+        RecordItem::Create(generation).record(record);
+        FamilyMember { record, generation }
     }
 }
 
-impl Drop for FamilyMember {
+impl<'a> Drop for FamilyMember<'a> {
     fn drop(&mut self) {
-        RecordItem::Drop(self.generation).record(self.family);
+        RecordItem::Drop(self.generation).record(self.record);
     }
 }
